cd74ce8521c63b2e06ac8247b64a22d3e9c941c3
qiqixiong <1016160938@qq.com>
2022-09-28 13:40:28 +0800

    mock

---
+++ mock/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const router = express.Router();
+
+app.post('/', function(req, res) {
+  res.send('this is my mock demo');
+});
+
+app.get('/', function(req, res) {
+  res.send('this is my mock get demo');
+});
+
+router.use("/user/", require('./user/index'));
+
+app.use("/api",router)
+
+app.listen(3080)
