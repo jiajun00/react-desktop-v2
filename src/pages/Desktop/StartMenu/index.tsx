66cef9e4dac2d12e42c35b42003c5dad06252249
qiqixiong <1016160938@qq.com>
2022-09-28 20:54:22 +0800

    startMenu

---
+++ src/pages/Desktop/StartMenu/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+interface Props {}
+
+const StartMenu: React.FC<Props> = props => {
+  return (
+    <div className={styles.startMenuBox}>
+      <div>asd</div>
+    </div>
+  )
+}
+
+export default StartMenu
